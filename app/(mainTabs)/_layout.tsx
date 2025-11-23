8f7a34418a18c72d331bba351903c05296d9a440
AbanobN <abanob.nabil.nagib@gmail.com>
2025-11-23 13:07:59 +0200

    Implement Project Strcutuer

---
+++ app/(mainTabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerTitleAlign: "center",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+
+      <Tabs.Screen
+        name="products"
+        options={{
+          title: "Products",
+        }}
+      />
+
+      <Tabs.Screen
+        name="shifts"
+        options={{
+          title: "Shifts",
+        }}
+      />
+
+      <Tabs.Screen
+        name="suppliers"
+        options={{
+          title: "Suppliers",
+        }}
+      />
+    </Tabs>
+  );
+}
